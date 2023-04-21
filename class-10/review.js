d71e831c4c22f0f92b047b6ab897e188fd705b53
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2023-04-21 14:58:19 -0700

    Added notes for review session

---
+++ class-10/review.js
@@ -0,0 +1,115 @@
+'use strict';
+
+// I want to make a function that will build a cupcake with a specified color for frosting, flavor
+
+// function makeCupcake(){
+//   console.log("You want a cupcake!");
+//   let color = prompt("What color frosting do you want?");
+//   let cupcake = prompt("What flavor cupcake do you want?");
+//   console.log("You would like a " + cupcake + " cupcake with " + color + " frosting");
+// }
+
+// makeCupcake();
+
+function makeCupcake(color, flavor){
+  console.log("You want a cupcake!");
+  console.log("You would like a " + flavor + " cupcake with " + color + " frosting");
+}
+
+makeCupcake();
+
+// ------------------------------
+
+// // I want to make a function that will build a cupcake with a specified color for frosting, flavor
+
+// // function makeCupcake(){
+// //   console.log("You want a cupcake!");
+// //   let color = prompt("What color frosting do you want?");
+// //   let cupcake = prompt("What flavor cupcake do you want?");
+// //   console.log("You would like a " + cupcake + " cupcake with " + color + " frosting");
+// // }
+
+// // makeCupcake();
+
+// function makeCupcake(color, flavor){
+//   console.log("You want a cupcake!");
+//   console.log("You would like a " + flavor + " cupcake with " + color + " frosting");
+// }
+
+// makeCupcake();
+
+// if (this is true){do this code here} else if (){} else {do this instead}
+
+// if (this is true){
+//   execute this code;
+// } else if (this is true instead){
+//   do this code;
+// } else {
+//   do this if neither of the previous conditions were true
+// }
+
+// let x = 10;
+
+// if (x > 10){
+//   console.log("The value is greater than 10");
+// } else if (x < 10) {
+//   console.log("The value is less than 10");
+// } else if (x == 10) {
+//   console.log("The value IS equivalent to 10");
+// } else {
+//   console.log("Why are we here?!?!?!");
+// }
+
+// if (x > 10){
+//   console.log("The value is greater than 10");
+// }
+
+// if (x < 10){
+//   console.log("The value is less than 10");
+// }
+
+
+// while(this evaluates to true){execute this code}
+
+// let theSwitch = true;
+
+// while(theSwitch){
+//   console.log("uh oh");
+//   theSwitch = false;
+// }
+
+// let x = 0;
+
+// while (x < 10){
+//   console.log(x);
+//   x++;
+// }
+
+// for (let i = 0; i <= 100; i++){
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 8; i++) {
+//   console.log(i);
+// }
+// 8 iterations
+
+// for (let i = 0; i <= 8; i=i+2) {
+//   console.log(i);
+// }
+// 5 iterations
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i);
+// }
+// 7 iterations
+
+// for (let i = 0; i <= 8; i++) {
+//   console.log(i);
+// } // 9 iterations
+
+// let x = 0;
+// while (x > -1){
+//   console.log(x);
+//   x++;
+// }
---
+++ class-10/review.md
@@ -0,0 +1,14 @@
+# Review Notes For Class 10
+
+## Resources
+
+* [replit](https://replit.com/)
+* [codepen.io](https://codepen.io/pen/)
+
+## What should we talk about?
+
+* Loops
+  * Which loop for which scenario?
+    * Quiz 8 - talked about loops, watch the recording
+* Functions
+* Conditional Statements/Logic
